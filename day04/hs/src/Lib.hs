f46d85cadfd67645b494c601fc0e0eb7d57cc506
Vesco Claudio <claudio.vesco@gmail.com>
2022-12-04 07:40:58 +0100

    day04

--- day04/hs/src/Lib.hs
+++ day04/hs/src/Lib.hs
@@ -1,26 +1,45 @@
 module Lib
-    ( someFunc, solve1, solve2
+    ( someFunc, solve1, solve2, parse, input, Data
     ) where
 
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
+import Data.Bifunctor
 
 someFunc :: IO ()
 someFunc = do
   start <- getCurrentTime
-  i <- input
-  let p1 = solve1 i :: Int
+  i <- parse <$> input
+  let p1 = solve1 i
   putStrLn $ "part 1: " ++ show p1
-  let p2 = solve2 i :: Integer
+  let p2 = solve2 i
   putStrLn $ "part 2: " ++ show p2
   end <- getCurrentTime
   let delta = diffUTCTime end start
   putStrLn $ "delta: " ++ show delta
 
+type Data = [((Int, Int), (Int, Int))]
+
 input :: IO String
 input = readFile "../input"
 
-solve1 :: String -> a
-solve1 = error "todo!"
+parse :: String -> Data
+parse = fmap parseL . lines
+  where parseL = bimap parseS (parseS . drop 1) . break (== ',')
+        parseS = bimap read (read . drop 1) . break (== '-')
 
-solve2 :: String -> a
-solve2 = error "todo!"
+solve :: (((Int, Int), (Int, Int)) -> Bool) -> Data -> Int
+solve test = length . filter test
+
+solve1 :: Data -> Int
+solve1 = solve contains'
+  where contains' (a, b) = a `contains` b || b `contains` a
+
+solve2 :: Data -> Int
+solve2 = solve overlap'
+  where overlap' (a, b) = a `overlap` b || b `overlap` a
+
+contains :: Ord a => (a, a) -> (a, a) -> Bool
+contains (al, ah) (bl, bh) = bl >= al && bl <= ah && bh >= al && bh <= ah
+
+overlap :: Ord a => (a, a) -> (a, a) -> Bool
+overlap  (al, ah) (bl, bh) = bl >= al && bl <= ah || bh >= al && bh <= ah
--- day04/rs/src/lib.rs
+++ day04/rs/src/lib.rs
@@ -1,15 +1,54 @@
+use std::str;
+
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref INPUT: &'static str = include_str!("../../input");
 }
 
-pub fn solve_1(_input: &str) -> usize {
-    todo!()
+struct Section(u32, u32);
+
+impl Section {
+    fn contains(&self, Section(bl, bh): &Self) -> bool {
+        let Section(al, ah) = *self;
+        (al..=ah).contains(bl) && (al..=ah).contains(bh)
+    }
+
+    fn overlap(&self, Section(bl, bh): &Self) -> bool {
+        let Section(al, ah) = *self;
+        (al..=ah).contains(bl) || (al..=ah).contains(bh)
+    }
 }
 
-pub fn solve_2(_input: &str) -> usize {
-    todo!()
+impl str::FromStr for Section {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Section, Self::Err> {
+        let invalid_number = |_| "invalid number";
+        s.split_once('-').ok_or("- not found").and_then(|(l, h)| {
+            Ok(Section(
+                l.parse().map_err(invalid_number)?,
+                h.parse().map_err(invalid_number)?,
+            ))
+        })
+    }
+}
+
+fn parse(line: &str) -> (Section, Section) {
+    let (a, b) = line.split_once(',').unwrap();
+    (a.parse().unwrap(), b.parse().unwrap())
+}
+
+fn solve<F: Fn(&Section, &Section) -> bool>(input: &str, test: F) -> usize {
+    input.lines().map(parse).filter(|(a, b)| test(a, b)).count()
+}
+
+pub fn solve_1(input: &str) -> usize {
+    solve(input, |a, b| a.contains(b) || b.contains(a))
+}
+
+pub fn solve_2(input: &str) -> usize {
+    solve(input, |a, b| a.overlap(b) || b.overlap(a))
 }
 
 pub fn part_1() -> usize {
@@ -25,18 +64,16 @@ mod tests {
     use super::*;
 
     lazy_static! {
-        static ref INPUT: &'static str = r#"XX"#;
+        static ref EXAMPLE1: &'static str = include_str!("../../example1");
     }
 
     #[test]
     fn same_results_1() {
-        // assert_eq!(solve_1(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_1(&EXAMPLE1), 2);
     }
 
     #[test]
     fn same_results_2() {
-        // assert_eq!(solve_2(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_2(&EXAMPLE1), 4);
     }
 }
