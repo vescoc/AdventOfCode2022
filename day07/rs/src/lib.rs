08a7d74610948ad6c99d3b98a92f60806ab05d9e
Vesco Claudio <claudio.vesco@gmail.com>
2022-12-07 09:08:16 +0100

    day 07

--- day07/rs/src/lib.rs
+++ day07/rs/src/lib.rs
@@ -1,22 +1,110 @@
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref INPUT: &'static str = include_str!("../../input");
 }
 
-pub fn solve_1(_input: &str) -> usize {
-    todo!()
+#[derive(Debug)]
+enum Node<'a> {
+    Dir(&'a str),
+    File(&'a str, u32),
 }
 
-pub fn solve_2(_input: &str) -> usize {
-    todo!()
+fn parse_filesystem(input: &str) -> HashMap<Vec<&str>, u32> {
+    let mut filesystem = HashMap::new();
+    let mut cwd = Vec::new();
+
+    for line in input.lines() {
+        let mut parts = line.split_whitespace();
+        match parts.next() {
+            Some("$") => match parts.next() {
+                Some("cd") => {
+                    match parts.next() {
+                        Some("/") => cwd.clear(),
+                        Some("..") => {
+                            cwd.pop();
+                        }
+                        Some(dir) => cwd.push(dir),
+                        _ => panic!("invalid cd"),
+                    }
+
+                    filesystem
+                        .entry(cwd.clone())
+                        .or_insert_with(Vec::new);
+                },
+                Some("ls") => {}
+                _ => panic!("invalid command"),
+            },
+            Some("dir") => filesystem.get_mut(&cwd)
+                .unwrap()
+                .push(Node::Dir(parts.next().unwrap())),
+            Some(size) => filesystem.get_mut(&cwd)
+                .unwrap()
+                .push(Node::File(
+                    parts.next().unwrap(),
+                    size.parse().expect("invalid size"),
+                )),
+            _ => panic!("invalid node"),
+        }
+    }
+
+    visit(HashMap::new(), &filesystem, vec![])
 }
 
-pub fn part_1() -> usize {
+fn visit<'a>(
+    mut current: HashMap<Vec<&'a str>, u32>,
+    filesystem: &HashMap<Vec<&'a str>, Vec<Node<'a>>>,
+    cwd: Vec<&'a str>,
+) -> HashMap<Vec<&'a str>, u32> {
+    let mut sum = 0;
+    for entry in filesystem.get(&cwd).unwrap() {
+        match entry {
+            Node::File(_, size) => sum += size,
+            Node::Dir(dir) => {
+                let wd = {
+                    let mut wd = cwd.clone();
+                    wd.push(dir);
+                    wd
+                };
+                current = visit(current, filesystem, wd.clone());
+                sum += current[&wd];
+            }
+        }
+    }
+
+    current.insert(cwd, sum);
+
+    current
+}
+
+pub fn solve_1(input: &str) -> u32 {
+    parse_filesystem(input)
+        .values()
+        .filter(|size| **size <= 100_000)
+        .sum()
+}
+
+pub fn solve_2(input: &str) -> u32 {
+    let filesystem = parse_filesystem(input);
+
+    let used_space = filesystem[&vec![]];
+    let unused_space = 70_000_000 - used_space;
+    let need_space = 30_000_000 - unused_space;
+
+    filesystem
+        .into_iter()
+        .filter_map(|(_, size)| if size >= need_space { Some(size) } else { None })
+        .min()
+        .unwrap()
+}
+
+pub fn part_1() -> u32 {
     solve_1(&INPUT)
 }
 
-pub fn part_2() -> usize {
+pub fn part_2() -> u32 {
     solve_2(&INPUT)
 }
 
@@ -25,18 +113,16 @@ mod tests {
     use super::*;
 
     lazy_static! {
-        static ref INPUT: &'static str = r#"XX"#;
+        static ref EXAMPLE1: &'static str = include_str!("../../example1");
     }
 
     #[test]
     fn same_results_1() {
-        // assert_eq!(solve_1(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_1(&EXAMPLE1), 95437);
     }
 
     #[test]
     fn same_results_2() {
-        // assert_eq!(solve_2(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_2(&EXAMPLE1), 24933642);
     }
 }
