6e9e637bd99e8215d6910f71e2c7785bd8868648
Vesco Claudio <claudio.vesco@gmail.com>
2022-12-08 17:55:48 +0100

    fmt

--- day08/rs/src/lib.rs
+++ day08/rs/src/lib.rs
@@ -17,70 +17,62 @@ pub fn solve_1(input: &str) -> usize {
         .enumerate()
         .flat_map(|(y, row)| {
             let mut max = 0;
-            row
+            row.iter().enumerate().filter_map(move |(x, &v)| {
+                if v > max {
+                    max = v;
+                    Some((x, y))
+                } else {
+                    None
+                }
+            })
+        })
+        .chain(
+            forest // right to left
                 .iter()
                 .enumerate()
-                .filter_map(move |(x, &v)| {
-                    if v > max {
-                        max = v;
-                        Some((x, y))
-                    } else {
-                        None
-                    }
-                })
-        })
-        .chain(forest // right to left
-               .iter()
-               .enumerate()
-               .flat_map(|(y, row)| {
-                   let mut max = 0;
-                   row
-                       .iter()
-                       .enumerate()
-                       .rev()
-                       .filter_map(move |(x, &v)| {
-                           if v > max {
-                               max = v;
-                               Some((x, y))
-                           } else {
-                               None
-                           }
-                       })
-               }))
-        .chain((0..forest[0].len()) // top to bottom
-               .flat_map(|x| {
-                   let mut max = 0;
-                   forest
-                       .iter()
-                       .enumerate()
-                       .filter_map(move |(y, row)| {
-                           let v = row[x];
-                           if v > max {
-                               max = v;
-                               Some((x, y))
-                           } else {
-                               None
-                           }
-                       })
-               }))
-        .chain((0..forest[0].len()) // bottom to top
-               .flat_map(|x| {
-                   let mut max = 0;
-                   forest
-                       .iter()
-                       .enumerate()
-                       .rev()
-                       .filter_map(move |(y, row)| {
-                           let v = row[x];
-                           if v > max {
-                               max = v;
-                               Some((x, y))
-                           } else {
-                               None
-                           }
-                       })
-               }))
-        .collect::<HashSet::<_>>();
+                .flat_map(|(y, row)| {
+                    let mut max = 0;
+                    row.iter().enumerate().rev().filter_map(move |(x, &v)| {
+                        if v > max {
+                            max = v;
+                            Some((x, y))
+                        } else {
+                            None
+                        }
+                    })
+                }),
+        )
+        .chain(
+            (0..forest[0].len()) // top to bottom
+                .flat_map(|x| {
+                    let mut max = 0;
+                    forest.iter().enumerate().filter_map(move |(y, row)| {
+                        let v = row[x];
+                        if v > max {
+                            max = v;
+                            Some((x, y))
+                        } else {
+                            None
+                        }
+                    })
+                }),
+        )
+        .chain(
+            (0..forest[0].len()) // bottom to top
+                .flat_map(|x| {
+                    let mut max = 0;
+                    forest.iter().enumerate().rev().filter_map(move |(y, row)| {
+                        let v = row[x];
+                        if v > max {
+                            max = v;
+                            Some((x, y))
+                        } else {
+                            None
+                        }
+                    })
+                }),
+        )
+        .collect::<HashSet<_>>();
 
     set.len()
 }
@@ -141,15 +133,13 @@ pub fn solve_2(input: &str) -> usize {
         .iter()
         .enumerate()
         .flat_map(|(y, row)| {
-            row.iter()
-                .enumerate()
-                .map(move |(x, &v)| {
-                    if x == 0 || y == 0 || x == mx || y == my {
-                        0
-                    } else {
-                        top(v, x, y) * bottom(v, x, y) * left(v, x, y) * right(v, x, y)
-                    }
-                })
+            row.iter().enumerate().map(move |(x, &v)| {
+                if x == 0 || y == 0 || x == mx || y == my {
+                    0
+                } else {
+                    top(v, x, y) * bottom(v, x, y) * left(v, x, y) * right(v, x, y)
+                }
+            })
         })
         .max()
         .unwrap()
