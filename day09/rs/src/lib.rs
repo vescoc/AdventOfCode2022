7082dfb233a9d9dcee4c49459c62a9dc2ad8c251
Vesco Claudio <claudio.vesco@gmail.com>
2022-12-09 09:51:14 +0100

    day 09

--- day09/rs/src/lib.rs
+++ day09/rs/src/lib.rs
@@ -1,15 +1,61 @@
+use std::collections::HashSet;
+
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref INPUT: &'static str = include_str!("../../input");
 }
 
-pub fn solve_1(_input: &str) -> usize {
-    todo!()
+fn follow((hx, hy): &(i32, i32), &(tx, ty): &(i32, i32)) -> (i32, i32) {
+    let (dx, dy) = (hx - tx, hy - ty);
+
+    match (dx.abs(), dy.abs()) {
+        (a, 0) if a <= 1 => (tx, ty),
+        (_, 0) => (tx + dx.signum(), ty),
+        (0, a) if a <= 1 => (tx, ty),
+        (0, _) => (tx, ty + dy.signum()),
+        (a, b) if a + b <= 2 => (tx, ty),
+        _ => (tx + dx.signum(), ty + dy.signum()),
+    }
 }
 
-pub fn solve_2(_input: &str) -> usize {
-    todo!()
+pub fn solve<const SIZE: usize>(input: &str) -> usize {
+    let mut positions = HashSet::new();
+
+    let mut rope = [(0, 0); SIZE];
+
+    for line in input.lines() {
+        let (direction, moves) = line.split_once(' ').expect("invalid move format");
+        let (dx, dy) = match direction {
+            "U" => (0, -1),
+            "D" => (0, 1),
+            "L" => (-1, 0),
+            "R" => (1, 0),
+            _ => panic!("invalid direction"),
+        };
+
+        let moves = moves.parse::<i32>().expect("invalid moves");
+        for _ in 0..moves {
+            rope[0].0 += dx;
+            rope[0].1 += dy;
+
+            for i in 1..rope.len() {
+                rope[i] = follow(&rope[i - 1], &rope[i]);
+            }
+
+            positions.insert(rope[SIZE - 1]);
+        }
+    }
+
+    positions.len()
+}
+
+pub fn solve_1(input: &str) -> usize {
+    solve::<2>(input)
+}
+
+pub fn solve_2(input: &str) -> usize {
+    solve::<10>(input)
 }
 
 pub fn part_1() -> usize {
@@ -25,18 +71,22 @@ mod tests {
     use super::*;
 
     lazy_static! {
-        static ref INPUT: &'static str = r#"XX"#;
+        static ref EXAMPLE1: &'static str = include_str!("../../example1");
+        static ref EXAMPLE2: &'static str = include_str!("../../example2");
     }
 
     #[test]
     fn same_results_1() {
-        // assert_eq!(solve_1(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_1(&EXAMPLE1), 13);
     }
 
     #[test]
-    fn same_results_2() {
-        // assert_eq!(solve_2(&INPUT), 666);
-        todo!();
+    fn same_results_2_1() {
+        assert_eq!(solve_2(&EXAMPLE1), 1);
+    }
+
+    #[test]
+    fn same_results_2_2() {
+        assert_eq!(solve_2(&EXAMPLE2), 36);
     }
 }
