3503eed00123b751bb4b75cc20622e0c77c3b96e
Vesco Claudio <claudio.vesco@gmail.com>
2022-12-11 19:36:11 +0100

    day 11

--- day11/hs/src/Lib.hs
+++ day11/hs/src/Lib.hs
@@ -1,16 +1,26 @@
 module Lib
-    ( someFunc, solve1, solve2
+    ( someFunc, solve1, solve2, parse, input, Input
     ) where
 
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
+import Data.Bifunctor
+import Data.Array.IArray
+import Data.List (partition, sortBy)
+
+data Operation = Add Int | Multiply Int | Square
+  deriving Show
+
+type MonkeyState = ([Int], Int)
+type MonkeyInfo = (Operation, Int, Int, Int)
+type Input = (Array Int MonkeyInfo, Array Int MonkeyState)
 
 someFunc :: IO ()
 someFunc = do
   start <- getCurrentTime
-  i <- input
-  let p1 = solve1 i :: Int
+  i <- parse <$> input
+  let p1 = solve1 i
   putStrLn $ "part 1: " ++ show p1
-  let p2 = solve2 i :: Integer
+  let p2 = solve2 i
   putStrLn $ "part 2: " ++ show p2
   end <- getCurrentTime
   let delta = diffUTCTime end start
@@ -19,8 +29,46 @@ someFunc = do
 input :: IO String
 input = readFile "../input"
 
-solve1 :: String -> a
-solve1 = error "todo!"
+parse :: String -> Input
+parse = bimap array' array' . unzip . parse' . lines
+  where parse' [] = []
+        parse' xs = (uncurry (:) . bimap (parseM . fmap words) (parse' . drop 1) . break (== "")) xs
+        array' xs = array (0, length xs - 1) $ zip [0..] xs
+        parseM [["Monkey", _],
+                "Starting":"items:":items,
+                ["Operation:", "new", "=", "old", op, value],
+                ["Test:", "divisible", "by", divisor],
+                ["If", "true:", "throw", "to", "monkey", tt],
+                ["If", "false:", "throw", "to", "monkey", ft]] = ((readOp op value, read divisor, read tt, read ft), (readItems items, 0))
+        parseM _ = error "invalid monkey"
+        readItems = map (read . takeWhile (/= ','))
+        readOp "+" value = Add $ read value
+        readOp "*" "old" = Square
+        readOp "*" value = Multiply $ read value
+        readOp _ _ = error "invalid operation"
 
-solve2 :: String -> a
-solve2 = error "todo!"
+solve :: Int -> (Int -> Int) -> Input -> Int
+solve n d (info, state) = product $ take 2 $ sortBy (flip compare) $ map snd $ elems $ (!!n) $ iterate (round' d info) state
+
+solve1 :: Input -> Int
+solve1 = solve 20 (`div` 3)
+
+solve2 :: Input -> Int
+solve2 (info, state) = let d = product $ map (\(_, t, _, _) -> t) $ elems info
+                       in solve 10000 (`mod` d) (info, state)
+
+round' :: (Int -> Int) -> Array Int MonkeyInfo -> Array Int MonkeyState -> Array Int MonkeyState
+round' d info = step (assocs info)
+  where step :: [(Int, MonkeyInfo)] -> Array Int MonkeyState -> Array Int MonkeyState
+        step [] state = state
+        step ((i, (op, test, tt, ft)):xs) state = step xs state'
+          where (items, c) = state!i
+                (ts, fs) = partition ((== 0) . (`mod` test)) $ map (d . eval op) items
+                state' = state // [(i, ([], c + length items)), (tt, append tt ts), (ft, append ft fs)]
+                append t ss = let (ss', c') = state!t
+                              in (ss' ++ ss, c')
+
+eval :: Operation -> Int -> Int
+eval (Add value) item = value + item
+eval (Multiply value) item = value * item
+eval Square item = item * item
--- day11/rs/src/lib.rs
+++ day11/rs/src/lib.rs
@@ -1,22 +1,159 @@
+use std::collections::VecDeque;
+use std::str;
+
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref INPUT: &'static str = include_str!("../../input");
 }
 
-pub fn solve_1(_input: &str) -> usize {
-    todo!()
+#[derive(Debug)]
+enum Operation {
+    Add(u64),
+    Multiply(u64),
+    Square,
 }
 
-pub fn solve_2(_input: &str) -> usize {
-    todo!()
+impl Operation {
+    fn eval(&self, value: u64) -> u64 {
+        match &self {
+            Operation::Add(other) => value + other,
+            Operation::Multiply(other) => value * other,
+            Operation::Square => value * value,
+        }
+    }
 }
 
-pub fn part_1() -> usize {
+#[derive(Debug)]
+struct Monkey {
+    #[allow(dead_code)]
+    id: usize,
+    items: VecDeque<u64>,
+    operation: Operation,
+    test: u64,
+    true_target: usize,
+    false_target: usize,
+    inspections: u64,
+}
+
+impl str::FromStr for Monkey {
+    type Err = &'static str;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let invalid_number = |_| "invalid number";
+
+        let mut lines = input.lines();
+
+        let id = lines.next().ok_or("no monkey id")?;
+        let id = id["Monkey ".len()..id.len() - 1]
+            .parse()
+            .map_err(invalid_number)?;
+
+        let items = lines.next().ok_or("no items")?;
+        let items = items["  Starting items: ".len()..]
+            .split(", ")
+            .map(|item| item.parse())
+            .collect::<Result<VecDeque<_>, _>>()
+            .map_err(invalid_number)?;
+
+        let operation = lines.next().ok_or("no operation")?;
+        let operation = match &operation[0.."  Operation: new = old * ".len()] {
+            "  Operation: new = old + " => Operation::Add(
+                operation["  Operation: new = old * ".len()..]
+                    .parse()
+                    .map_err(invalid_number)?,
+            ),
+            "  Operation: new = old * "
+                if &operation["  Operation: new = old * ".len()..] == "old" =>
+            {
+                Operation::Square
+            }
+            "  Operation: new = old * " => Operation::Multiply(
+                operation["  Operation: new = old * ".len()..]
+                    .parse()
+                    .map_err(invalid_number)?,
+            ),
+            _ => Err("invalid operation")?,
+        };
+
+        let test = lines.next().ok_or("no test")?;
+        let test = test["  Test: divisible by ".len()..]
+            .parse()
+            .map_err(invalid_number)?;
+
+        let true_target = lines.next().ok_or("no true target")?;
+        let true_target = true_target["    If true: throw to monkey ".len()..]
+            .parse()
+            .map_err(invalid_number)?;
+
+        let false_target = lines.next().ok_or("no false target")?;
+        let false_target = false_target["    If false: throw to monkey ".len()..]
+            .parse()
+            .map_err(invalid_number)?;
+
+        Ok(Monkey {
+            id,
+            items,
+            operation,
+            test,
+            true_target,
+            false_target,
+            inspections: 0,
+        })
+    }
+}
+
+fn solve<FD: Fn(u64) -> u64, F: FnOnce(&[Monkey]) -> FD>(input: &str, n: usize, f: F) -> u64 {
+    let mut monkeys = input
+        .split("\n\n")
+        .map(|monkey| monkey.parse::<Monkey>().expect("invalid monkey"))
+        .collect::<Vec<_>>();
+
+    let divisor = f(&monkeys);
+
+    for _r in 0..n {
+        for i in 0..monkeys.len() {
+            while let Some(item) = monkeys[i].items.pop_front() {
+                monkeys[i].inspections += 1;
+
+                let item = monkeys[i].operation.eval(item);
+                let item = divisor(item);
+                let target = if item % monkeys[i].test == 0 {
+                    monkeys[i].true_target
+                } else {
+                    monkeys[i].false_target
+                };
+                monkeys[target].items.push_back(item);
+            }
+        }
+    }
+
+    let mut inspections = monkeys
+        .iter()
+        .map(|monkey| monkey.inspections)
+        .collect::<Vec<_>>();
+
+    inspections.sort_by(|a, b| b.cmp(a));
+
+    inspections[0] * inspections[1]
+}
+
+pub fn solve_1(input: &str) -> u64 {
+    solve(input, 20, |_| |item| item / 3)
+}
+
+pub fn solve_2(input: &str) -> u64 {
+    solve(input, 10_000, |monkeys| {
+        let divisor: u64 = monkeys.iter().map(|monkey| monkey.test).product();
+        move |item| item % divisor
+    })
+}
+
+pub fn part_1() -> u64 {
     solve_1(&INPUT)
 }
 
-pub fn part_2() -> usize {
+pub fn part_2() -> u64 {
     solve_2(&INPUT)
 }
 
@@ -25,18 +162,16 @@ mod tests {
     use super::*;
 
     lazy_static! {
-        static ref INPUT: &'static str = r#"XX"#;
+        static ref EXAMPLE1: &'static str = include_str!("../../example1");
     }
 
     #[test]
     fn same_results_1() {
-        // assert_eq!(solve_1(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_1(&EXAMPLE1), 10605);
     }
 
     #[test]
     fn same_results_2() {
-        // assert_eq!(solve_2(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_2(&EXAMPLE1), 2713310158);
     }
 }
