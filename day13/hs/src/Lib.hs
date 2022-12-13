824b439b8af88a94d25c30f164f121322d37d026
Vesco Claudio <claudio.vesco@gmail.com>
2022-12-13 18:47:50 +0100

    day 13

--- day13/hs/src/Lib.hs
+++ day13/hs/src/Lib.hs
@@ -1,16 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc, solve1, solve2
+    ( someFunc, solve1, solve2, parse, input, Input
     ) where
 
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
+import Data.Maybe
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Aeson
+import Data.Vector (toList)
+import Data.List (sort)
+
+data List = I Int | L [List]
+  deriving (Show, Eq)
+
+type Input = [List]
 
 someFunc :: IO ()
 someFunc = do
   start <- getCurrentTime
-  i <- input
-  let p1 = solve1 i :: Int
+  i <- parse <$> input
+  let p1 = solve1 i
   putStrLn $ "part 1: " ++ show p1
-  let p2 = solve2 i :: Integer
+  let p2 = solve2 i
   putStrLn $ "part 2: " ++ show p2
   end <- getCurrentTime
   let delta = diffUTCTime end start
@@ -19,8 +31,27 @@ someFunc = do
 input :: IO String
 input = readFile "../input"
 
-solve1 :: String -> a
-solve1 = error "todo!"
+parse :: String -> Input
+parse = mapMaybe (parseL . B.pack) . lines
+  where parseL = decode'
 
-solve2 :: String -> a
-solve2 = error "todo!"
+solve1 :: Input -> Int
+solve1 = sum . map fst . filter ((/= GT) . snd) . zip [1..] . map (uncurry compare) . chunks
+  where chunks [] = []
+        chunks (a:b:xs) = (a, b) : chunks xs
+        chunks _ = error "invalid chunk"
+
+solve2 :: Input -> Int
+solve2 = product . map fst . filter ((`elem` [k2, k6]) . snd) . zip [1..] . sort . (k2 :) . (k6 :)
+  where k2 = L [L [I 2]]
+        k6 = L [L [I 6]]
+
+instance FromJSON List where
+  parseJSON (Array a) = L <$> mapM parseJSON (toList a)
+  parseJSON n = I <$> parseJSON n
+
+instance Ord List where
+  (I a) `compare` (I b) = a `compare` b
+  a@(I _) `compare` b@(L _) = L [a] `compare` b
+  a@(L _) `compare` b@(I _) = a `compare` L [b]
+  (L a) `compare` (L b) = mconcat (zipWith compare a b) <> (length a `compare` length b)
--- day13/rs/src/lib.rs
+++ day13/rs/src/lib.rs
@@ -1,15 +1,79 @@
+use std::cmp::Ordering;
+use std::iter;
+
+use serde::Deserialize;
+
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref INPUT: &'static str = include_str!("../../input");
 }
 
-pub fn solve_1(_input: &str) -> usize {
-    todo!()
+#[derive(Deserialize, Debug, Clone)]
+#[serde(untagged)]
+enum List {
+    Int(u32),
+    List(Vec<List>),
 }
 
-pub fn solve_2(_input: &str) -> usize {
-    todo!()
+fn is_valid(packets: &str) -> bool {
+    let mut packets = packets.lines();
+    let packet1: List =
+        serde_json::from_str(packets.next().expect("packet 1 not found")).expect("invalid list");
+    let packet2: List =
+        serde_json::from_str(packets.next().expect("packet 2 not found")).expect("invalid list");
+
+    cmp(packet1, packet2) != Ordering::Greater
+}
+
+fn cmp(packet1: List, packet2: List) -> Ordering {
+    match (packet1, packet2) {
+        (List::Int(a), List::Int(b)) => a.cmp(&b),
+        (a @ List::Int(_), b @ List::List(_)) => cmp(List::List(vec![a]), b),
+        (a @ List::List(_), b @ List::Int(_)) => cmp(a, List::List(vec![b])),
+        (List::List(a), List::List(b)) => {
+            let (l1, l2) = (a.len(), b.len());
+            a
+                .into_iter()
+                .zip(b.into_iter())
+                .fold(Ordering::Equal, |acc, (a, b)| acc.then_with(|| cmp(a, b)))
+                .then_with(|| l1.cmp(&l2))
+        }
+    }
+}
+
+pub fn solve_1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .enumerate()
+        .filter_map(|(i, packets)| if is_valid(packets) { Some(i + 1) } else { None })
+        .sum()
+}
+
+pub fn solve_2(input: &str) -> usize {
+    let decoder_key_2 = serde_json::from_str("[[2]]").unwrap();
+    let decoder_key_6 = serde_json::from_str("[[6]]").unwrap();
+
+    let mut packets = input
+        .lines()
+        .filter_map(|line| {
+            if line.is_empty() {
+                None
+            } else {
+                Some((false, serde_json::from_str(line).expect("invalid list")))
+            }
+        })
+        .chain(iter::once((true, decoder_key_2)))
+        .chain(iter::once((true, decoder_key_6)))
+        .collect::<Vec<(bool, List)>>();
+
+    packets.sort_by(|(_, a), (_, b)| cmp(a.clone(), b.clone()));
+
+    packets
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, (k, _))| if k { Some(i + 1) } else { None })
+        .product()
 }
 
 pub fn part_1() -> usize {
@@ -25,18 +89,16 @@ mod tests {
     use super::*;
 
     lazy_static! {
-        static ref INPUT: &'static str = r#"XX"#;
+        static ref EXAMPLE1: &'static str = include_str!("../../example1");
     }
 
     #[test]
     fn same_results_1() {
-        // assert_eq!(solve_1(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_1(&EXAMPLE1), 13);
     }
 
     #[test]
     fn same_results_2() {
-        // assert_eq!(solve_2(&INPUT), 666);
-        todo!();
+        assert_eq!(solve_2(&EXAMPLE1), 140);
     }
 }
